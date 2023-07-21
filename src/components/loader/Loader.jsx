0ff3a288761ad4cd19496252a103f9acc90af1de
Maciej Bartoszewski <bartoszewski.maciek@gmail.com>
2023-07-21 14:30:01 +0200

    commit

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import { Watch } from 'react-loader-spinner';
+import styles from '../styles/styles.css';
+import PropTypes from 'prop-types';
+
+export class Loader extends Component {
+  render() {
+    const { isLoading } = this.props;
+    return (
+      <div className={styles.loader}>
+        <Watch
+          height="80"
+          width="80"
+          radius="48"
+          color="#4fa94d"
+          ariaLabel="watch-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={isLoading}
+        />
+      </div>
+    );
+  }
+}
+Loader.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
